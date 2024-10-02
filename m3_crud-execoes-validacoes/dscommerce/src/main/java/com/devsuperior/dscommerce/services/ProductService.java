7627682712bb744230280baa8cf86f41fe1377e2
Anderson Sipriano <Andsipriano@gmail.com>
2024-10-02 17:00:14 -0300

    Paginação

--- m3_crud-execoes-validacoes/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ m3_crud-execoes-validacoes/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.services;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +22,8 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public List<ProductDTO> findAll() {
-        List<ProductDTO> list = repository.findAll().stream().map(ProductDTO::new).toList();
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<ProductDTO> list = repository.findAll(pageable).map(ProductDTO::new);
         return list;
     }
 
