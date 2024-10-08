de39b231f20ffcc3c2b46976b757f27e9ef7510b
Anderson Sipriano <Andsipriano@gmail.com>
2024-10-08 18:52:13 -0300

    Inserindo novo produto com post

--- m3_crud-execoes-validacoes/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ m3_crud-execoes-validacoes/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -25,6 +26,20 @@ public class ProductService {
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<ProductDTO> list = repository.findAll(pageable).map(ProductDTO::new);
         return list;
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
     }
 
 }
