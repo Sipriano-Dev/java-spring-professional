160c8e52b313f58d26468d1cca8a2926a0f7584e
Anderson Sipriano <Andsipriano@gmail.com>
2024-10-09 17:07:30 -0300

    Criando exceções de serviço customizadas

--- m3_crud-execoes-validacoes/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ m3_crud-execoes-validacoes/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.services;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,7 +20,8 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        return new ProductDTO(repository.findById(id).get());
+        return new ProductDTO(repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado!")));
     }
 
     @Transactional(readOnly = true)
