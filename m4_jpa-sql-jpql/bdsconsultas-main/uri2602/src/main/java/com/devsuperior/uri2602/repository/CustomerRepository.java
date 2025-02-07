76b55e5ac8625786e233bb9f58ec2de9504eb281
Anderson Sipriano <Andsipriano@gmail.com>
2025-02-07 20:43:06 -0300

    URI 2602 Sprint Boot SQL

---
+++ m4_jpa-sql-jpql/bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/repository/CustomerRepository.java
@@ -0,0 +1,19 @@
+package com.devsuperior.uri2602.repository;
+
+import com.devsuperior.uri2602.entities.Customer;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT name "
+            + "FROM customers "
+            + "WHERE state= :state ")
+    List<CustomerMinProjection> search1(String state);
+
+}
