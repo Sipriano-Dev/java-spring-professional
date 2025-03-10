7970ef846ef806bedcb420b7342d60964972f88f
Anderson Sipriano <Andsipriano@gmail.com>
2025-03-10 17:40:45 -0300

    BCrypt password encoder

--- m5_oauth2-jwt/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ m5_oauth2-jwt/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$GaRSn/FMHp90e2mr2fTmn.71KZPHlb31H6J75kG/yD3qh.t9295Gu');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$GaRSn/FMHp90e2mr2fTmn.71KZPHlb31H6J75kG/yD3qh.t9295Gu');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
