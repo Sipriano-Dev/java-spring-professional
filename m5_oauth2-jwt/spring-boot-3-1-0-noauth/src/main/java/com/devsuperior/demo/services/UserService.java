1c5c910bd4f92fdf8a2546ef6d8bb50e339c9282
Anderson Sipriano <Andsipriano@gmail.com>
2025-03-19 11:54:07 -0300

    Controle de acesso por perfil e rota

--- m5_oauth2-jwt/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
+++ m5_oauth2-jwt/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
@@ -28,8 +28,8 @@ public class UserService implements UserDetailsService {
         }
 
         User user = new User();
-        user.setName(result.getFirst().getUsername());
-        user.setPassword(result.getFirst().getPassword());
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
         for (UserDetailsProjection projection : result) {
             user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
