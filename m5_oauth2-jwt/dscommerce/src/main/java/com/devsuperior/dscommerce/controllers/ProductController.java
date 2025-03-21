140ae7b17a4dc57b10cd84d9d4b0b41a57a157e5
Anderson Sipriano <Andsipriano@gmail.com>
2025-03-21 17:33:07 -0300

    Salvando um pedido

--- m5_oauth2-jwt/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ m5_oauth2-jwt/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -51,7 +51,7 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
